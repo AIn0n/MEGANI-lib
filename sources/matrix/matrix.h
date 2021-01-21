f744553f1dce57d83377ab0e408a25aed0660d72
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2021-01-21 17:50:54 +0100

    Working with doxygen documentation

--- sources/matrix/matrix.h
+++ sources/matrix/matrix.h
@@ -1,15 +1,29 @@
+/**
+*   @file  matrix.h
+*   @brief Header file for matrix math.
+*/
 #ifndef _MATRIX_H_
 #define _MATRIX_H_
 
-#include <stdint.h> //for uintN_t
+#include <stdint.h> //for uintN_t - better portability
 
-#define NN_TYPE float //type of variables with neural values
+/** @def    NN_TYPE
+*   @brief  Type of variables with neural values.
+*/
+#define NN_TYPE float 
 
+/** @struct matrix_t 
+*   @brief  Structure with matrix, data and size.
+*
+*   I designed this struct as interpreted 1 dimensional array to make it as fast and small
+*   as possible. To change the data type in the matrix, change NN_TYPE.
+*   @see NN_TYPE
+*/
 typedef struct 
 {
-    NN_TYPE* arr;
-    uint32_t x;
-    uint32_t y;
+    NN_TYPE* arr;   /**< Array with matrix data. For type see NN_TYPE */
+    uint32_t x;     /**< matrix width */
+    uint32_t y;     /**< matrix height*/
 } 
 matrix_t;
 
