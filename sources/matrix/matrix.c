5c009e937ccdc265174cacead9e02c5c93486278
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2021-01-22 10:56:22 +0100

    Added matrix_create func

--- sources/matrix/matrix.c
+++ sources/matrix/matrix.c
@@ -1,4 +1,22 @@
+#include <stdlib.h>
 #include "matrix.h"
 
-//test func
-void foo(void) {}
+matrix_t* 
+matrix_create(uint32_t x, uint32_t y)
+{
+    if(!x || !y) return NULL;
+    
+    matrix_t* output = (matrix_t *)calloc(1, sizeof(output));
+    if(output == NULL) return NULL;
+
+    output->arr = (NN_TYPE *)calloc(x * y, sizeof(NN_TYPE));
+    if(output->arr == NULL) 
+    { 
+        free(output); 
+        return NULL;
+    }
+
+    output->x = x;
+    output->y = y;
+    return output;
+}
--- sources/matrix/matrix.h
+++ sources/matrix/matrix.h
@@ -8,12 +8,14 @@
 #include <stdint.h> //for uintN_t - better portability
 
 /** @def    NN_TYPE
-*   @brief  Type of variables with neural values.
+*   @brief  Type of variables which contain neural values.
+*
+*   By default it is "double".
 */
-#define NN_TYPE float 
+#define NN_TYPE double 
 
 /** @struct matrix_t 
-*   @brief  Structure with matrix, data and size.
+*   @brief  Structure with matrix data and size.
 *
 *   I designed this struct as interpreted 1 dimensional array to make it as fast and small
 *   as possible. To change the data type in the matrix, change NN_TYPE.
@@ -24,7 +26,20 @@ typedef struct
     NN_TYPE* arr;   /**< Array with matrix data. For type see NN_TYPE */
     uint32_t x;     /**< matrix width */
     uint32_t y;     /**< matrix height*/
-} 
+}
 matrix_t;
+
+/** matrix_create
+ *  @brief function create new matrix and return pointer to it. 
+ * 
+ *  Function get size of output matrix <x, y>, check input data,
+ *  allocate memory (with calloc) for it and return pointer. 
+ * 
+ *  On success, pointex to matrix is returned. If errors occurs, function return NULL;
+ * 
+ *  @param [in] x output matrix width.
+ *  @param [in] y output matrix height
+ */
+matrix_t* matrix_create(uint32_t x, uint32_t y);
 
 #endif
