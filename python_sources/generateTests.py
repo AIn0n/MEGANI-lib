7c7f2eb574bb9eb0aa205b7bc5e306d875d6bbb0
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-03-15 22:41:08 +0100

    Used code formatter on python source files

--- python_sources/generateTests.py
+++ python_sources/generateTests.py
@@ -3,207 +3,253 @@ import numpy as np
 import random
 import subprocess
 
-DELTA = .0001
+DELTA = 0.0001
 
-#	matrix create tests
+# 	matrix create tests
 
 gen = TestsGenerator()
 for pair in [[0, 0], [1, 0], [0, 1]]:
-	gen.genTest('mx_create',
-		'\tmx_t *a = mx_create({}, {});\n'.format(*pair) +
-		genAssert('a != NULL'))
+    gen.genTest(
+        "mx_create",
+        "\tmx_t *a = mx_create({}, {});\n".format(*pair) + genAssert("a != NULL"),
+    )
 
 x, y = random.randint(1, 16), random.randint(1, 16)
-gen.genTest('mx_create',
-	f'\tmx_t *a = mx_create({x}, {y});\n' +
-	genAssert('a == NULL') + 
-	genAssert(f'a->x != {x} || a->y != {y} || a->size != {x * y}') +
-	'\tmx_destroy(a);')
+gen.genTest(
+    "mx_create",
+    f"\tmx_t *a = mx_create({x}, {y});\n"
+    + genAssert("a == NULL")
+    + genAssert(f"a->x != {x} || a->y != {y} || a->size != {x * y}")
+    + "\tmx_destroy(a);",
+)
 
-#	matrix multiplication tests
+# 	matrix multiplication tests
 
-for t in ('0', 'A', 'B', 'BOTH'):
-	for n in range(5):
-		x1, y1 = random.randint(1, 16), random.randint(1, 16)
-		x2, y2 = {
-			'0'	: (random.randint(1, 16), x1), 
-			'A'	: (random.randint(1, 16), y1),
-			'B' 	: (x1, random.randint(1, 16)), 
-			'BOTH'	: (y1, random.randint(1, 16))}[t]
+for t in ("0", "A", "B", "BOTH"):
+    for n in range(5):
+        x1, y1 = random.randint(1, 16), random.randint(1, 16)
+        x2, y2 = {
+            "0": (random.randint(1, 16), x1),
+            "A": (random.randint(1, 16), y1),
+            "B": (x1, random.randint(1, 16)),
+            "BOTH": (y1, random.randint(1, 16)),
+        }[t]
 
-		x3, y3 = {'0':(x2,y1),'A':(x2,x1),'B':(y2,y1),'BOTH':(y2,x1)}[t]
-		a = np.random.randint(-254, high = 254, size = (y1, x1))
-		b = np.random.randint(-254, high = 254, size = (y2, x2))
-		expected = np.matmul(
-			np.transpose(a) if t in ('A', 'BOTH') else a, 
-			np.transpose(b) if t in ('B', 'BOTH') else b).flatten()
+        x3, y3 = {"0": (x2, y1), "A": (x2, x1), "B": (y2, y1), "BOTH": (y2, x1)}[t]
+        a = np.random.randint(-254, high=254, size=(y1, x1))
+        b = np.random.randint(-254, high=254, size=(y2, x2))
+        expected = np.matmul(
+            np.transpose(a) if t in ("A", "BOTH") else a,
+            np.transpose(b) if t in ("B", "BOTH") else b,
+        ).flatten()
 
-		gen.genTest('mx_mp' if t == '0' else 'mx_mp (transposed)',
-			genStaticMxDec(a, 'a') +
-			genStaticMxDec(b, 'b') +
-			genStaticEmptyMxDec((y3, x3), 'res') +
-			genStaticListDec(expected, 'exp') +
-			f'\tmx_mp(a, b, &res, {t});\n\n' +
-			genMxComp('res', 'exp', DELTA))
+        gen.genTest(
+            "mx_mp" if t == "0" else "mx_mp (transposed)",
+            genStaticMxDec(a, "a")
+            + genStaticMxDec(b, "b")
+            + genStaticEmptyMxDec((y3, x3), "res")
+            + genStaticListDec(expected, "exp")
+            + f"\tmx_mp(a, b, &res, {t});\n\n"
+            + genMxComp("res", "exp", DELTA),
+        )
 
-#	matrix hadamard product tests
-
-for n in range(5):
-	x, y = random.randint(1, 16), random.randint(1, 16)
-	a = np.random.randint(-254, high = 256, size = (y, x))
-	b = np.random.randint(-254, high = 256, size = (y, x))
-	expected = np.multiply(a, b).flatten()
-	gen.genTest('mx_hadamard',
-		genStaticMxDec(a, 'a') +
-		genStaticMxDec(b, 'b') +
-		genStaticEmptyMxDec((y, x), 'res') +
-		genStaticListDec(expected, 'exp') +
-		f'\tmx_hadamard(a, b, &res);\n' +
-		genMxComp('res', 'exp', DELTA))
-
-#	matrix substraction
+# 	matrix hadamard product tests
 
 for n in range(5):
-	x, y = random.randint(1, 16), random.randint(1, 16)
-	a = np.random.randint(-254, high = 254, size = (y, x))
-	b = np.random.randint(-254, high = 254, size = (y, x))
-	expected = (a - b).flatten()
-	gen.genTest('mx_substraction',
-		genStaticMxDec(a, 'a') +
-		genStaticMxDec(b, 'b') +
-		genStaticEmptyMxDec((y, x), 'res') +
-		genStaticListDec(expected, 'exp') +
-		f'\tmx_sub(a, b, &res);\n' +
-		genMxComp('res', 'exp', DELTA))
+    x, y = random.randint(1, 16), random.randint(1, 16)
+    a = np.random.randint(-254, high=256, size=(y, x))
+    b = np.random.randint(-254, high=256, size=(y, x))
+    expected = np.multiply(a, b).flatten()
+    gen.genTest(
+        "mx_hadamard",
+        genStaticMxDec(a, "a")
+        + genStaticMxDec(b, "b")
+        + genStaticEmptyMxDec((y, x), "res")
+        + genStaticListDec(expected, "exp")
+        + f"\tmx_hadamard(a, b, &res);\n"
+        + genMxComp("res", "exp", DELTA),
+    )
 
-#	matrix multiply by number
+# 	matrix substraction
 
 for n in range(5):
-	x, y = random.randint(1, 16), random.randint(1, 16)
-	a = np.random.randint(-254, high = 254, size = (y, x))
-	num = random.randint(-16, 16)
-	expected = (a * num).flatten()
-	gen.genTest('mx_mp_num',
-		genStaticMxDec(a, 'a') +
-		genStaticListDec(expected, 'exp') +
-		f'\tmx_mp_num(&a, {num});\n' +
-		genMxComp('a', 'exp', DELTA))
+    x, y = random.randint(1, 16), random.randint(1, 16)
+    a = np.random.randint(-254, high=254, size=(y, x))
+    b = np.random.randint(-254, high=254, size=(y, x))
+    expected = (a - b).flatten()
+    gen.genTest(
+        "mx_substraction",
+        genStaticMxDec(a, "a")
+        + genStaticMxDec(b, "b")
+        + genStaticEmptyMxDec((y, x), "res")
+        + genStaticListDec(expected, "exp")
+        + f"\tmx_sub(a, b, &res);\n"
+        + genMxComp("res", "exp", DELTA),
+    )
 
-#	matrix hadamard product with lambda
+# 	matrix multiply by number
 
-gen.appendCode('\nMX_TYPE foo(MX_TYPE a) {return (a > 4) ? 0 : a;}\n')
+for n in range(5):
+    x, y = random.randint(1, 16), random.randint(1, 16)
+    a = np.random.randint(-254, high=254, size=(y, x))
+    num = random.randint(-16, 16)
+    expected = (a * num).flatten()
+    gen.genTest(
+        "mx_mp_num",
+        genStaticMxDec(a, "a")
+        + genStaticListDec(expected, "exp")
+        + f"\tmx_mp_num(&a, {num});\n"
+        + genMxComp("a", "exp", DELTA),
+    )
+
+# 	matrix hadamard product with lambda
+
+gen.appendCode("\nMX_TYPE foo(MX_TYPE a) {return (a > 4) ? 0 : a;}\n")
 
 x, y = random.randint(1, 16), random.randint(1, 16)
-a = np.random.randint(-254, high = 254, size = (y, x))
-b = np.random.randint(-254, high = 254, size = (y, x))
+a = np.random.randint(-254, high=254, size=(y, x))
+b = np.random.randint(-254, high=254, size=(y, x))
 new_b = np.array([[0 if x > 4 else x for x in y] for y in b])
 expected = (a * new_b).flatten()
 
-gen.genTest('mx_hadam_lambda',
-	genStaticMxDec(a, 'a') +
-	genStaticMxDec(b, 'b') +
-	f'\tmx_hadam_lambda(&a, b, (&foo));\n' +
-	genStaticListDec(expected, 'exp') +
-	genMxComp('a', 'exp', DELTA))
+gen.genTest(
+    "mx_hadam_lambda",
+    genStaticMxDec(a, "a")
+    + genStaticMxDec(b, "b")
+    + f"\tmx_hadam_lambda(&a, b, (&foo));\n"
+    + genStaticListDec(expected, "exp")
+    + genMxComp("a", "exp", DELTA),
+)
 
-#	neural network create
+# 	neural network create
 
 for n in range(5):
-	inputSize = random.randint(1, 16)
-	batchSize = random.randint(1, 16)
-	denseSize1 = random.randint(1, 16)
-	denseSize2 = random.randint(1, 16)
-	maxTmp = max([inputSize * denseSize1, denseSize1 * denseSize2])
-	gen.genTest('nn_create',
-
-	#initializer of neural network
-	f'''
+    inputSize = random.randint(1, 16)
+    batchSize = random.randint(1, 16)
+    denseSize1 = random.randint(1, 16)
+    denseSize2 = random.randint(1, 16)
+    maxTmp = max([inputSize * denseSize1, denseSize1 * denseSize2])
+    gen.genTest(
+        "nn_create",
+        # initializer of neural network
+        f"""
 		nn_t *nn = nn_create({inputSize}, {batchSize}, 0.01);
 		LAYER_DENSE(nn, {denseSize1}, RELU, 0.1, 0.2);
-		LAYER_DENSE(nn, {denseSize2}, NO_FUNC, 0.1, 0.2);\n''' +
+		LAYER_DENSE(nn, {denseSize2}, NO_FUNC, 0.1, 0.2);\n"""
+        +
+        # check size of first layer
+        genAssert(
+            f"nn->layers[0].out->x != {denseSize1} || nn->layers[0].out->y != {batchSize}"
+        )
+        + """\n\tdense_data_t *ptr = nn->layers[0].data;\n"""
+        + genAssert(f"ptr->val->x != {inputSize} || ptr->val->y != {denseSize1}")
+        +
+        # check size of second layer
+        genAssert("nn->layers[1].out == NULL")
+        + genAssert(
+            f"nn->layers[1].out->x != {denseSize2} || nn->layers[1].out->y != {batchSize}"
+        )
+        + """\n\tptr = (dense_data_t *) nn->layers[1].data;\n"""
+        + genAssert(f"ptr->val->x != {denseSize1} || ptr->val->y != {denseSize2}")
+        +
+        # check size of temporary matrix stored in neural network struct
+        genAssert(f"nn->temp->size != {maxTmp}") + "\tnn_destroy(nn);\n",
+    )
 
-	# check size of first layer
-		genAssert(f'nn->layers[0].out->x != {denseSize1} || nn->layers[0].out->y != {batchSize}') +
-		'''\n\tdense_data_t *ptr = nn->layers[0].data;\n''' +
-		genAssert(f'ptr->val->x != {inputSize} || ptr->val->y != {denseSize1}') +
+# 	neural network predict
 
-	# check size of second layer
-		genAssert('nn->layers[1].out == NULL') +
-		genAssert(f'nn->layers[1].out->x != {denseSize2} || nn->layers[1].out->y != {batchSize}') +
-		'''\n\tptr = (dense_data_t *) nn->layers[1].data;\n''' +
-		genAssert(f'ptr->val->x != {denseSize1} || ptr->val->y != {denseSize2}') +
-
-	# check size of temporary matrix stored in neural network struct
-		genAssert(f'nn->temp->size != {maxTmp}') +
-		'\tnn_destroy(nn);\n')
-
-#	neural network predict
-
-gen.genTest('nn_predict',
-	genStaticMxDec(np.array([[8.5, 0.65, 1.2]]), 'input') + '''
+gen.genTest(
+    "nn_predict",
+    genStaticMxDec(np.array([[8.5, 0.65, 1.2]]), "input")
+    + """
 	nn_t *n = nn_create(3, 1, 0.01);
 	LAYER_DENSE(n, 3, NO_FUNC, 0.0, 0.0);
 	LAYER_DENSE(n, 3, NO_FUNC, 0.0, 0.0);
-	dense_data_t *ptr = (n->layers[0].data);\n''' +	
-	genStaticListDec([.1, .2, -.1, -.1, .1, .9, .1, .4, .1], 'val0') +
-	genStaticListDec([.3, 1.1, -.3, .1, .2, .0, .0, 1.3, .1], 'val1') +
-	genListCpy('val0', 'ptr->val->arr','ptr->val->size') +
-	'\tptr = n->layers[1].data;\n' +
-	genListCpy('val1', 'ptr->val->arr', 'ptr->val->size') +
-	'\tnn_predict(n, &input);\n' +
-	genStaticListDec([.2135, .145, .5065], 'expected') +
-	genMxComp('(*n->layers[1].out)', 'expected', DELTA) +
-	'\tnn_destroy(n);\n')
+	dense_data_t *ptr = (n->layers[0].data);\n"""
+    + genStaticListDec([0.1, 0.2, -0.1, -0.1, 0.1, 0.9, 0.1, 0.4, 0.1], "val0")
+    + genStaticListDec([0.3, 1.1, -0.3, 0.1, 0.2, 0.0, 0.0, 1.3, 0.1], "val1")
+    + genListCpy("val0", "ptr->val->arr", "ptr->val->size")
+    + "\tptr = n->layers[1].data;\n"
+    + genListCpy("val1", "ptr->val->arr", "ptr->val->size")
+    + "\tnn_predict(n, &input);\n"
+    + genStaticListDec([0.2135, 0.145, 0.5065], "expected")
+    + genMxComp("(*n->layers[1].out)", "expected", DELTA)
+    + "\tnn_destroy(n);\n",
+)
 
-gen.genTest('nn_predict',
-'''	nn_t *nn = nn_create(1, 1, 0.01);
+gen.genTest(
+    "nn_predict",
+    """	nn_t *nn = nn_create(1, 1, 0.01);
 	LAYER_DENSE(nn, 3, RELU, 0.0, 0.0);
-	LAYER_DENSE(nn, 1, NO_FUNC, 0.0, 0.0);\n''' +
-	genStaticListDec([.1, -.1, .1], 'val0') +
-	genStaticListDec([.3, 1.1, -.3], 'val1') +
-	'\tdense_data_t *ptr = (dense_data_t *) nn->layers->data;\n' +
-	genListCpy('val0', 'ptr->val->arr', 'ptr->val->size') +
-	'\n\tptr = (dense_data_t *) nn->layers[1].data;\n' +
-	genListCpy('val1', 'ptr->val->arr', 'ptr->val->size') +
-	genStaticMxDec(np.array([[8.5]]), 'input') +
-	'\tnn_predict(nn, &input);\n' +
-	genAssert(f'nn->layers[1].out->arr[0] > {DELTA} || nn->layers[1].out->arr[0] < -{DELTA}') +
-	'\tnn_destroy(nn);\n')
+	LAYER_DENSE(nn, 1, NO_FUNC, 0.0, 0.0);\n"""
+    + genStaticListDec([0.1, -0.1, 0.1], "val0")
+    + genStaticListDec([0.3, 1.1, -0.3], "val1")
+    + "\tdense_data_t *ptr = (dense_data_t *) nn->layers->data;\n"
+    + genListCpy("val0", "ptr->val->arr", "ptr->val->size")
+    + "\n\tptr = (dense_data_t *) nn->layers[1].data;\n"
+    + genListCpy("val1", "ptr->val->arr", "ptr->val->size")
+    + genStaticMxDec(np.array([[8.5]]), "input")
+    + "\tnn_predict(nn, &input);\n"
+    + genAssert(
+        f"nn->layers[1].out->arr[0] > {DELTA} || nn->layers[1].out->arr[0] < -{DELTA}"
+    )
+    + "\tnn_destroy(nn);\n",
+)
 
 ##	neural network predict
 
-gen.genTest('nn_fit',
-'''   	nn_t* nn = nn_create(3, 4, 0.01);
+gen.genTest(
+    "nn_fit",
+    """   	nn_t* nn = nn_create(3, 4, 0.01);
 	LAYER_DENSE(nn, 3, RELU, 0.0, 0.0);
-	LAYER_DENSE(nn, 3, NO_FUNC, 0.0, 0.0);\n''' +
-	genStaticMxDec(np.array([
-		[8.5, .65, 1.2], 
-		[9.5, .8, 1.3], 
-		[9.9, .8, .5],
-		[9.0, .9, 1.0]]), 'input') +
-	genStaticListDec([.1, .2, -.1, -.1, .1, .9, .1, .4, .1], 'val0') +
-	genStaticListDec([.3, 1.1, -.3, .1, .2, .0, .0, 1.3, .1], 'val1') +
-	'\tdense_data_t* ptr = nn->layers->data;\n' +
-	genListCpy('val0', 'ptr->val->arr', 'ptr->val->size') + 
-	'\n\tptr = nn->layers[1].data;\n' +
-	genListCpy('val1', 'ptr->val->arr', 'ptr->val->size') +
-	genStaticMxDec(np.array([
-		[0.1, 1.0, 0.1], 
-		[0.0, 1.0, 0.0], 
-		[0.0, 0.0, 0.1], 
-		[0.1, 1.0, 0.2]]),'out') +
-	'\tnn_fit(nn, &input, &out);\n' +
-	genStaticListDec(
-		[.118847, .201724, -.0977926,
-		-.190674, .0930147, .886871, 
-		.093963, .399449, .0994944], 'exp_val0') +
-	genStaticListDec(
-		[.29901, 1.09916, -.301627,
-		.123058, .205844, .0328309,
-		-.0096053, 1.29716, .0863697], 'exp_val1') +
-	genMxComp('(* ptr->val)', 'exp_val1', DELTA) +
-	'\tptr = (dense_data_t *) nn->layers[0].data;\n' +
-	genMxComp('(* ptr->val)', 'exp_val0', DELTA) +
-	'\tnn_destroy(nn);\n')
+	LAYER_DENSE(nn, 3, NO_FUNC, 0.0, 0.0);\n"""
+    + genStaticMxDec(
+        np.array([[8.5, 0.65, 1.2], [9.5, 0.8, 1.3], [9.9, 0.8, 0.5], [9.0, 0.9, 1.0]]),
+        "input",
+    )
+    + genStaticListDec([0.1, 0.2, -0.1, -0.1, 0.1, 0.9, 0.1, 0.4, 0.1], "val0")
+    + genStaticListDec([0.3, 1.1, -0.3, 0.1, 0.2, 0.0, 0.0, 1.3, 0.1], "val1")
+    + "\tdense_data_t* ptr = nn->layers->data;\n"
+    + genListCpy("val0", "ptr->val->arr", "ptr->val->size")
+    + "\n\tptr = nn->layers[1].data;\n"
+    + genListCpy("val1", "ptr->val->arr", "ptr->val->size")
+    + genStaticMxDec(
+        np.array([[0.1, 1.0, 0.1], [0.0, 1.0, 0.0], [0.0, 0.0, 0.1], [0.1, 1.0, 0.2]]),
+        "out",
+    )
+    + "\tnn_fit(nn, &input, &out);\n"
+    + genStaticListDec(
+        [
+            0.118847,
+            0.201724,
+            -0.0977926,
+            -0.190674,
+            0.0930147,
+            0.886871,
+            0.093963,
+            0.399449,
+            0.0994944,
+        ],
+        "exp_val0",
+    )
+    + genStaticListDec(
+        [
+            0.29901,
+            1.09916,
+            -0.301627,
+            0.123058,
+            0.205844,
+            0.0328309,
+            -0.0096053,
+            1.29716,
+            0.0863697,
+        ],
+        "exp_val1",
+    )
+    + genMxComp("(* ptr->val)", "exp_val1", DELTA)
+    + "\tptr = (dense_data_t *) nn->layers[0].data;\n"
+    + genMxComp("(* ptr->val)", "exp_val0", DELTA)
+    + "\tnn_destroy(nn);\n",
+)
 
-gen.save('sources/main.c')
+gen.save("sources/main.c")
--- python_sources/testsGenerator.py
+++ python_sources/testsGenerator.py
@@ -1,100 +1,106 @@
-#	---=== predefined strings ===---
+# 	---=== predefined strings ===---
 
-_TEST_ASSERT	= '''\tif ({}) {{\n\t\tprintf("---- ERROR! (line %i)\\n", __LINE__);\n\t\treturn 1;\n\t}}\n'''
-_TEST_START 	= '''\nstatic int\ntest{number}(void)\n{{\n\tputs("-- TEST {number} -- {name}");\n\n'''
-_TEST_END	= '''\tputs("---- OK!");\n\treturn 0;\n}\n'''
-_START_MAIN	= '''\nint\nmain(void) {\n\tint (* test_funcs[]) (void) = {'''
+_TEST_ASSERT = """\tif ({}) {{\n\t\tprintf("---- ERROR! (line %i)\\n", __LINE__);\n\t\treturn 1;\n\t}}\n"""
+_TEST_START = """\nstatic int\ntest{number}(void)\n{{\n\tputs("-- TEST {number} -- {name}");\n\n"""
+_TEST_END = """\tputs("---- OK!");\n\treturn 0;\n}\n"""
+_START_MAIN = """\nint\nmain(void) {\n\tint (* test_funcs[]) (void) = {"""
 
-_INCLUDE = '''#include <stdio.h>
+_INCLUDE = """#include <stdio.h>
 #include "mx.h"
 #include "nn.h"
 #include "dense.h"
-'''
+"""
 
-_END_MAIN ='''}};
+_END_MAIN = """}};
 	const int test_size = {numOfTests};
 	int failed = 0;
 	for (int i = 0; i < test_size; ++i)
 		failed += (*test_funcs[i])();
 	
 	printf("%i of %i tests failed\\n", failed, test_size);
-	return failed;\n}}'''
+	return failed;\n}}"""
 
-#	---=== generators of static declarations ===---
+# 	---=== generators of static declarations ===---
 
 # generate static matrix declaration
-def genStaticMxDec(mx, mxName :str) -> str:
-	return \
-f'''	mx_t {mxName} = {{.x = {mx.shape[1]}, .y = {mx.shape[0]}, .size = {mx.size}}};
-	MX_TYPE {mxName}_arr[] = {{\n''' +\
-	''.join('\t\t' + ''.join(f'{x}, ' for x in y) + '\n' for y in mx) +\
-	f'\n\t}};\n\t{mxName}.arr = {mxName}_arr;\n\n'
+def genStaticMxDec(mx, mxName: str) -> str:
+    return (
+        f"""	mx_t {mxName} = {{.x = {mx.shape[1]}, .y = {mx.shape[0]}, .size = {mx.size}}};
+	MX_TYPE {mxName}_arr[] = {{\n"""
+        + "".join("\t\t" + "".join(f"{x}, " for x in y) + "\n" for y in mx)
+        + f"\n\t}};\n\t{mxName}.arr = {mxName}_arr;\n\n"
+    )
+
 
 # generate static empty matrix declaration
-def genStaticEmptyMxDec(shape, mxName :str) -> str:
-	size = shape[0] * shape[1]
-	return \
-f'''	mx_t {mxName} = {{.x = {shape[1]}, .y = {shape[0]}, .size = {size}}};
-	MX_TYPE {mxName}_arr[{size}];''' +\
-	f'\n\t{mxName}.arr = {mxName}_arr;\n\n'
+def genStaticEmptyMxDec(shape, mxName: str) -> str:
+    size = shape[0] * shape[1]
+    return (
+        f"""	mx_t {mxName} = {{.x = {shape[1]}, .y = {shape[0]}, .size = {size}}};
+	MX_TYPE {mxName}_arr[{size}];"""
+        + f"\n\t{mxName}.arr = {mxName}_arr;\n\n"
+    )
+
 
 # generate static list declaration
-def genStaticListDec(l, lName :str) -> str:
-	return \
-	f'\tMX_TYPE {lName}[] = {{' + ''.join(f'{n}, ' for n in l) + '};\n\n'
+def genStaticListDec(l, lName: str) -> str:
+    return f"\tMX_TYPE {lName}[] = {{" + "".join(f"{n}, " for n in l) + "};\n\n"
 
-#	---=== generators of macro-like functions ===---
+
+# 	---=== generators of macro-like functions ===---
 
 # generate matrix comparison
-#	arguments:
-#	* mx - matrix name as string
-#	* l  - list name as string
-#	* delta - max acceptable difference between expected and got value
-def genMxComp(mx :str, l :str, delta :float) -> str:
-	return \
-f'''	for (MX_SIZE i = 0; i < {mx}.size; ++i)
+# 	arguments:
+# 	* mx - matrix name as string
+# 	* l  - list name as string
+# 	* delta - max acceptable difference between expected and got value
+def genMxComp(mx: str, l: str, delta: float) -> str:
+    return f"""	for (MX_SIZE i = 0; i < {mx}.size; ++i)
 		if ({mx}.arr[i] > {l}[i] + {delta} || {mx}.arr[i] < {l}[i] - {delta}) {{
 			printf("---- ERROR! (line %i)\\n------ expected -> %f, got -> %f\\n", __LINE__, {l}[i], {mx}.arr[i]);
 			return 1;
 		}}
-'''
+"""
+
 
 # generate matrix/list copy
-#	arguments:
-#	* src - source list name as string
-#	* dest - destination list name as string
-#	* size - size as string
-def genListCpy(src :str, dest :str, size :str) -> str:
-	return \
-f'''	for (MX_SIZE i = 0; i < {size}; ++i)
+# 	arguments:
+# 	* src - source list name as string
+# 	* dest - destination list name as string
+# 	* size - size as string
+def genListCpy(src: str, dest: str, size: str) -> str:
+    return f"""	for (MX_SIZE i = 0; i < {size}; ++i)
 		{dest}[i] = {src}[i];
-'''
+"""
+
 
 # generate static if-like assertion
-def genAssert(code :str) -> str:
-	return _TEST_ASSERT.format(code)
+def genAssert(code: str) -> str:
+    return _TEST_ASSERT.format(code)
+
 
 class TestsGenerator:
-	def __init__(self) -> None:
-	    self.testCounter = 0
-	    self.code = _INCLUDE
-	
-	def genTest(self, testName :str, code :str) -> None:
-		self.code +=\
-		_TEST_START.format(number = self.testCounter, name = testName) +\
-		code + _TEST_END
-		self.testCounter += 1
+    def __init__(self) -> None:
+        self.testCounter = 0
+        self.code = _INCLUDE
 
-	def appendCode(self, code :str) -> None:
-		self.code += code
+    def genTest(self, testName: str, code: str) -> None:
+        self.code += (
+            _TEST_START.format(number=self.testCounter, name=testName)
+            + code
+            + _TEST_END
+        )
+        self.testCounter += 1
 
-	def __genMain(self) -> None:
-		self.code += _START_MAIN
-		self.code += ''.join(
-			f'test{n},' for n in range(self.testCounter))
-		self.code += _END_MAIN.format(numOfTests = self.testCounter)
+    def appendCode(self, code: str) -> None:
+        self.code += code
 
-	def save(self, filepath) -> None:
-		self.__genMain()
-		with open(filepath, 'w') as f:
-			print(self.code, file = f)
+    def __genMain(self) -> None:
+        self.code += _START_MAIN
+        self.code += "".join(f"test{n}," for n in range(self.testCounter))
+        self.code += _END_MAIN.format(numOfTests=self.testCounter)
+
+    def save(self, filepath) -> None:
+        self.__genMain()
+        with open(filepath, "w") as f:
+            print(self.code, file=f)
