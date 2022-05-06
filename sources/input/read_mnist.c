098c322eed510f1ef83314af9b24c41331e315a4
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-05-07 01:24:45 +0200

    teoreticlly mnist reader should working, still needs testing

--- sources/input/read_mnist.c
+++ sources/input/read_mnist.c
@@ -26,7 +26,7 @@ const mx_iterator empty_iterator = {.list = NULL, .size = 0, .curr = 0};
 #define I32_LEN 32
 
 mx_iterator
-read_idx3(mx_t list[], const char *filename, mx_size batch_len, uint8_t vertical)
+read_idx3(const char *filename, mx_size batch_len, uint8_t vertical)
 {
 	FILE *f = fopen(filename, "rb");
 	if (f == NULL || batch_len == 0)
@@ -43,13 +43,46 @@ read_idx3(mx_t list[], const char *filename, mx_size batch_len, uint8_t vertical
 	    || fread(&height,	1, I32_LEN, f) != I32_LEN
 	    || fread(&width,	1, I32_LEN, f) != I32_LEN
 	    || !width || !height || !size)
-		goto err;
+		goto err_close_file;
 
 
 	reverse_bytes_int32(&size);
 	reverse_bytes_int32(&height);
 	reverse_bytes_int32(&width);
-err:
+
+	const mx_size batch_size = width * height * batch_len;
+	const mx_size all_batch = size / batch_size;
+
+	uint8_t *tmp = malloc(batch_size);
+	if (tmp == NULL)
+		goto err_close_file;
+
+	mx_t *list = malloc(all_batch * sizeof(*list));
+	if (list == NULL)
+		goto err_free_tmp;
+
+	const mx_size x = (vertical) ? batch_size : width * height;
+	const mx_size y = (vertical) ? 1 : batch_len;
+
+	mx_t *new = list;
+	for (int i = 0; i < all_batch; ++i, ++new) {
+		new = mx_create(x, y);
+		if (new == NULL || fread(tmp, batch_size, 1, f) != batch_size) {
+			for (int j = i - 1; j > -1; --j)
+				free(new--);
+			free(list);
+			goto err_free_tmp;
+		}
+		for (int n = 0; n < batch_size; ++n)
+			new[i].arr[n] = (mx_type)(tmp[n] / 255);
+	}
+	free(tmp);
+	fclose(f);
+	return (mx_iterator) {.list = list, .curr = 0, .size = all_batch};
+
+err_free_tmp:
+	free(tmp);
+err_close_file:
 	fclose(f);
 	return empty_iterator;
 }
