48dccf777bf7745a301cdde73b971b8f98a4467a
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-05-09 23:43:31 +0200

    extracted logic from read_mnist function into another one

--- sources/input/read_mnist.c
+++ sources/input/read_mnist.c
@@ -21,8 +21,27 @@ reverse_bytes_int32(int32_t *n)
 
 /* In case of some errors in reading function I just return empty iterator */
 const mx_iterator empty_iterator = {.list = NULL, .size = 0, .curr = 0};
+
 #define I32_LEN 4
 #define U8_LEN 1
+
+static inline uint8_t
+read_and_verify_header_idx3(FILE *f, int32_t *size, int32_t *height, int32_t *width)
+{
+	int32_t magic;
+	if (fread(&magic,	1, I32_LEN, f) != I32_LEN
+	    || fread(size,	1, I32_LEN, f) != I32_LEN
+	    || fread(height,	1, I32_LEN, f) != I32_LEN
+	    || fread(width,	1, I32_LEN, f) != I32_LEN)
+		return 1;
+
+	reverse_bytes_int32(&magic);
+	reverse_bytes_int32(size);
+	reverse_bytes_int32(height);
+	reverse_bytes_int32(width);
+	
+	return (magic != 2051 || !*width || !*height || !*size);
+}
 
 mx_iterator
 read_idx3(const char *filename, mx_size batch_len, uint8_t vertical)
@@ -30,44 +49,26 @@ read_idx3(const char *filename, mx_size batch_len, uint8_t vertical)
 	FILE *f = fopen(filename, "rb");
 	if (f == NULL || batch_len == 0)
 		return empty_iterator;
-	/* 
-	* read whole file header build with magic number, size, height width
-	* everything is done in single if to make this optimal and small
-	* also, I think that it can be helpful for function caching but idk
-	* am not cpu lol
-	*/
-	int32_t magic_num, size, height, width;
-	if (fread(&magic_num,	1, I32_LEN, f) != I32_LEN
-	    || fread(&size,	1, I32_LEN, f) != I32_LEN
-	    || fread(&height,	1, I32_LEN, f) != I32_LEN
-	    || fread(&width,	1, I32_LEN, f) != I32_LEN)
-		goto err_close_file;
 
-	reverse_bytes_int32(&magic_num);
-	reverse_bytes_int32(&size);
-	reverse_bytes_int32(&height);
-	reverse_bytes_int32(&width);
-	
-	if (magic_num != 2051 || !width || !height || !size)
-		goto err_close_file;
+	int32_t size, height, width;
+	if (read_and_verify_header_idx3(f, &size, &height, &width))
+		goto close_file_err;
 
-	const mx_size num_of_batches = size / batch_len;
-	const mx_size batch_cells = width * height * batch_len;
-
-	mx_t** list = malloc(sizeof(*list) * num_of_batches);
-	if (list == NULL)
-		goto err_close_file;
-
+	const mx_size num_of_batches 	= size / batch_len;
+	const mx_size batch_cells 	= width * height * batch_len;
 	const mx_size x = (vertical) ? batch_cells : (mx_size) (width * height);
 	const mx_size y = (vertical) ? 1 : batch_len;
 
-	uint8_t byte;
+	mx_t** list = malloc(sizeof(*list) * num_of_batches);
+	if (list == NULL)
+		goto close_file_err;
 	mx_size n;
 	for (n = 0; n < num_of_batches; ++n) {
 		list[n] = mx_create(x, y);
 		if (list[n] == NULL)
 			goto free_all_err;
 		for (mx_size i = 0; i < batch_cells; ++i) {
+			uint8_t byte;
 			if (fread(&byte, 1, U8_LEN, f) != U8_LEN)
 				goto free_all_err;
 			list[n]->arr[i] = (mx_type) byte / 255;
@@ -80,7 +81,7 @@ free_all_err:
 	for (int j = n; j > -1; --j)
 		mx_destroy(list[j]);
 	free(list);
-err_close_file:
+close_file_err:
 	fclose(f);
 	return empty_iterator;
 }
